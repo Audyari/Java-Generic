5edc9856cb4b3abc50a5f7be425be73af888d01d
Audyari Wiyono <audy123ari@gmail.com>
2024-07-02 16:26:30 +0700

    Generic Class

---
+++ src/GenericClass/GenericClassApp.java
@@ -0,0 +1,17 @@
+package GenericClass;
+
+public class GenericClassApp {
+
+    public static void main(String[] args) {
+
+        MyData<String> stringMyData = new MyData<String>("Audy");
+        MyData<Integer> integerMyData = new MyData<Integer>(10);
+
+        String stringValue = stringMyData.getData();
+        Integer integerValue = integerMyData.getData();
+
+        System.out.println(stringValue);
+        System.out.println(integerValue);
+
+    }
+}
