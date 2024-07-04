88bd6a2fb910cdcd8b87a814125de3af7671a338
Audyari Wiyono <audy123ari@gmail.com>
2024-07-04 10:46:23 +0700

    Covariant

---
+++ src/Covariant/ContravariantApp.java
@@ -0,0 +1,26 @@
+package Covariant;
+
+import GenericClass.MyData;
+
+public class ContravariantApp {
+
+
+    public static void main(String[] args) {
+
+        MyData<Object> objectMyData = new MyData<>("Eko");
+        objectMyData.setData(1000);
+
+        MyData<? super String> myData = objectMyData;
+
+        process(objectMyData);
+
+        System.out.println(objectMyData.getData());
+    }
+
+    public static void process(MyData<? super String> myData){
+        Object value = myData.getData();
+        System.out.println("Process parameter " + value);
+
+        myData.setData("Eko Kurniawan");
+    }
+}
